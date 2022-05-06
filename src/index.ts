d49a7bee4e091a77a5137dac427b6343126dd4ff
fujia <fujia.site@outlook.com>
2022-05-06 15:36:00 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import cp, { CommonSpawnOptions, ChildProcess } from 'child_process';
+
+export const isWin32 = process.platform === 'win32';
+
+export const spawn = (
+  command: string,
+  args: readonly string[],
+  options: CommonSpawnOptions
+): ChildProcess => {
+  // NOTE: In window OS, it need to execute command by cmd
+  const cmd = isWin32 ? 'cmd' : command;
+  const cmdArgs = isWin32 ? ['/c'].concat(command, args) : args;
+
+  return cp.spawn(cmd, cmdArgs, options || {});
+};
+
+export const spawnAsync = (
+  command: string,
+  args: readonly string[],
+  options: CommonSpawnOptions
+): Promise<number | null> => {
+  return new Promise((resolve, reject) => {
+    const cp = spawn(command, args, options);
+
+    cp.on('error', (err) => {
+      reject(err);
+    });
+
+    cp.on('exit', (chunk) => {
+      resolve(chunk);
+    });
+  });
+};
